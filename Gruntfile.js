3026390b8bd70fabd00da9ea0f6cabf5bf5017d4
pandeiro245 <nishiko@mindia.jp>
2013-11-02 22:30:56 +0900

    add grunt-contrib-cssmin

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,16 +2,25 @@ module.exports = function(grunt){
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     coffee: {
-        compile: {
-            files: {
-                'public/js/timecard-backbone.js': [
-                  'app/models/*.coffee', 
-                  'app/controllers/*.coffee', 
-                  'app/views/*.coffee', 
-                  'app/*.coffee'
-                ],
-            }
-        },
+      compile: {
+        files: {
+          'public/js/timecard-backbone.js': [
+            'app/models/*.coffee', 
+            'app/controllers/*.coffee', 
+            'app/views/*.coffee', 
+            'app/*.coffee'
+          ],
+        }
+      },
+    },
+    cssmin: {
+      combine: {
+        files: {
+          'public/css/timecard-backbone.css': [
+            'app/stylesheets/*css'
+          ],
+        }
+      }
     },
     watch: {
       doc: {
@@ -20,7 +29,8 @@ module.exports = function(grunt){
       }
     }
   });
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-coffee');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.registerTask('default', 'coffee'); 
+  grunt.registerTask('default', ['coffee', 'cssmin']); 
 }
